cdfd44f3e97b8f6c2386c10e4ecaf457fb52b882
LichuAcu <lichu.acuna@vercel.com>
2024-08-21 11:48:28 -0700

    Fix next server component modules in graph traversal

--- turbopack/crates/turbopack-core/src/chunk/module_id_strategies.rs
+++ turbopack/crates/turbopack-core/src/chunk/module_id_strategies.rs
@@ -46,10 +46,10 @@ impl ModuleIdStrategy for GlobalModuleIdStrategy {
     async fn get_module_id(self: Vc<Self>, ident: Vc<AssetIdent>) -> Result<Vc<ModuleId>> {
         let ident_string = ident.to_string().await?.clone_value();
         if let Some(module_id) = self.await?.module_id_map.get(&ident_string) {
-            dbg!(format!("Hit {}", ident.to_string().await?));
+            // dbg!(format!("Hit {}", ident.to_string().await?));
             return Ok(*module_id);
         }
-        dbg!(format!("Miss {}", ident.to_string().await?));
+        // dbg!(format!("Miss {}", ident.to_string().await?));
         Ok(ModuleId::String(
             hash_xxh3_hash64(ident.to_string().await?)
                 .to_string()
